e1a65cd5c3e05fd015fb3fd88f4dd12271f0fa46
ravirajutture1011 <rajutture1026@gmail.com>
2025-06-07 12:14:48 +0530

    reddis configuration completed

---
+++ server/utils/redis.js
@@ -0,0 +1,7 @@
+
+import Redis from "ioredis";
+
+const redis = new Redis("rediss://default:AWcsAAIjcDEzYmQ0YTgzNGI4MWY0YmUxYTBmYzg5Mzk0M2Y3ZDg0ZnAxMA@joint-puma-26412.upstash.io:6379"); // Use the Upstash URL from your .env
+
+export default redis;
+
